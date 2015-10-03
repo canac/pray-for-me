918a395b60f30d422f446d0a1a3ba1ab77f3990d
Caleb Cox <calebcox314@gmail.com>
2015-10-02 23:42:39 -0400

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name prayForMeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the prayForMeApp
+ */
+angular.module('prayForMeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
